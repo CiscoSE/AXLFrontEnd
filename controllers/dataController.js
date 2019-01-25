dc2c0f9a8f4f822d16a013b5e545f37973a43539
administrator <administrator@as-rtp-app01.pawhalen.net>
2019-01-25 14:33:40 -0500

    controllers

---
+++ controllers/dataController.js
@@ -0,0 +1,32 @@
+const db = require("../models");
+const path = require("path");
+
+module.exports = {
+
+    setSearchString: function(req, res) {
+        db.models.Result
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },  
+
+    startSearch: function(req, res) {
+        console.log("req.body in startSearch", req.body.searchId)
+        const spawn = require("child_process").spawn;
+        const pythonProcess = spawn('python',[path.join(__dirname, "../scripts/python/ccmdbsearch.py"), req.body.searchId, req.body.searchString])
+        res.status(202).send()
+
+        pythonProcess.stdout.on('data', (data) => {
+            console.log(data.toString());
+        
+        });
+    },
+
+    checkResult: function(req, res) {
+        console.log("req.body in checkResult", req.body.searchId)
+        db.models.Result
+            .findById(req.body.searchId)
+            .then(dbModel => res.send(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+}
